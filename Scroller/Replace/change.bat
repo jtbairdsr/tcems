e80838631a4876e55f28121fe57418201be74e55
Jonathan Baird <bai11006@byui.edu>
2015-02-02 13:41:21 -0700

    Remove scroller

--- Scroller/Replace/change.bat
+++
@@ -1,5 +0,0 @@
-echo off
-DEL "\\SCOUT\U$\DATA\Testing Common\Scroller\scrolling.html"
-COPY "\\SCOUT\U$\DATA\Testing Common\Scroller\Replace\scrolling.html" "\\SCOUT\U$\DATA\Testing Common\Scroller\scrolling.html"
-echo Go to Sis McPherson's office and hit F5 on the computer that controls the TV to update the display.
-pause
